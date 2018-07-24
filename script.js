cc3458011ba64aabc51bc50986b003244fbf0ea5
Rachel <racheleldridge@juno.com>
2018-07-24 19:23:51 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Display a dandelion-colored alert at the top of the page
-$(document).ready(function() {
-    $('#alert').click(function() {
-    //    Fade out the message when clicked
-        $('#alert').fadeOut(1000);
-            });
-        });
